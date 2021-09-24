38161c2f994ad0b7d8d06226561f0c7cc8877570
Jakob Adomeit <jakob.k.adomeit@web.de>
2021-09-24 23:58:10 +0100

    index.js - async await instead of callback

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 const express = require("express")
-const { readFile } = require("fs")
+const { readFile } = require("fs").promises
 const app = express()
 
-app.get("/", (request, response) => {
-    readFile("./home.html", "utf8", (err, html) => {
-        if (err) {
-            response.status(500).send("Sorry, out of order")
-        }
-        response.send(html)
-    })
+app.get("/", async (request, response) => {
+    response.send( await readFile("./home.html", "utf8") )
 })
 
 app.listen(process.env.PORT || 3000, () => console.log("app available on http://localhost:3000"))
